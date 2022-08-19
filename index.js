79d6924e3c7646f8afa4632713dc71ee40ff02c0
M.M <synchromatik@gmail.com>
2022-08-19 17:48:57 +0200

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/routes')
+const cors = require('cors')
 require('dotenv').config()
 
 // init app
@@ -10,6 +11,10 @@ const app = express()
 const morgan = require('morgan')
 
 app
+	.options('*', cors());
+
+app
+	.use(cors())
 	.use(morgan('dev'))
 	.use('/api', routes)
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const inputs = require('../models/senzori')
 const router = express.Router()
 
-router.get('/senzori', async (req, res) => {
+router.get('/senzori', async (req, res, next) => {
   try {
     senzori = await inputs.find({})
     res.send(senzori)
@@ -11,12 +11,12 @@ router.get('/senzori', async (req, res) => {
   }
 })
 
-router.get('/senzori/:from-:to', async (req, res) => {
+router.get('/senzori/:from-:to', async (req, res, next) => {
   try {
-    senzori = await inputs.find({'Temp': { $gt: req.params.from, $lt: req.params.to }}, 'Temp Date')
+    senzori = await inputs.find({'Temp': { $gt: req.params.from, $lt: req.params.to }}, 'Temp, Date')
     res.send(senzori)
   } catch {
-    res.send({error: err})
+    res.send({error: 'empty'})
   }
 })
 
